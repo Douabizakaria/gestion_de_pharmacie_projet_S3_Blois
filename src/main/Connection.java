cf1f3c929f1c7c520a5ed995b4760e1cac39965c
DOUABIZAKARIA <zakaria.douabi11@outlook.fr>
2021-12-04 17:05:56 +0100

    modification Classe Connection.java Aujourd'hui Réunion chez Moi et Momo à la maison

--- src/main/Connection.java
+++ src/main/Connection.java
@@ -8,7 +8,7 @@ public class Connection {
     public static java.sql.Connection connect(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost/pharmacie","root","");
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost/pharmacie","root","");// utilisation de PHPMyadmin
             if(con!=null)return con ;
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, e.getMessage(),"Error",2);

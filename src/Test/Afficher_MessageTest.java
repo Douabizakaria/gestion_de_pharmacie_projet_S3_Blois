761cc58ed0b473c6ef7c40cbb67e417c7fb6c102
Mehdimakoul <95297317+Mehdimakoul@users.noreply.github.com>
2021-12-13 23:00:36 +0100

    TESTS J UNIT
    tous les test successfully passé

---
+++ src/Test/Afficher_MessageTest.java
@@ -0,0 +1,61 @@
+/**
+ * @author Mehdi El makoul
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mehdi
+ */
+public class Afficher_MessageTest {
+    
+    public Afficher_MessageTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * 
+     * Test of main method, of class Afficher_Message.
+     */
+    @Test
+    public void Afficher_Message() {
+        
+    }
+
+    
+    
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+         Afficher_Message();
+                
+            
+        
+        fail("The test case is a prototype.");
+    }
+    
+}

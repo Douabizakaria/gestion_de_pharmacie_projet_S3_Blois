9bb0978d4d68a5d94e60ff00503ca65fdffc0d5d
ouiam tbibi <93492354+Ouiama@users.noreply.github.com>
2021-12-14 01:50:33 +0100

    Add files via upload

---
+++ src/Test/Renouvellement_validiteTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ouiam tbibi
+ */
+public class Renouvellement_validiteTest {
+    
+    public Renouvellement_validiteTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class Renouvellement_validite.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Renouvellement_validite.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
